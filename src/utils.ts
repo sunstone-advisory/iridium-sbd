40c4d7cb8d193f181b55b486f2c6f617302a7789
Brendon Stephens <brendon.stephens@sunstoneadvisory.com.au>
2022-06-06 15:57:16 +1000

    fix(ci): update feed name

--- src/utils.ts
+++ src/utils.ts
@@ -2,22 +2,22 @@ import usx from 'unishox2.siara.cc'
 
 /**
  * Compress a string using the Unishox 2 compression algorithm.
- * 
+ *
  * @param str
  * @returns {Buffer} the compress string as binary
  */
 export function compress (str: string) {
-    const uint8arr = new Uint8Array(str.length + 10)
-    const length =  usx.unishox2_compress_simple(str, str.length, uint8arr)
-    return Buffer.from(uint8arr.subarray(0, length))
+  const uint8arr = new Uint8Array(str.length + 10)
+  const length = usx.unishox2_compress_simple(str, str.length, uint8arr)
+  return Buffer.from(uint8arr.subarray(0, length))
 }
 
 /**
  * Decompress a buffer encoded with the Unishox 2 compression algorithm.
- * 
- * @param buffer 
+ *
+ * @param buffer
  * @returns {string} the decompressed string
  */
 export function decompress (buffer: Buffer) {
-    return usx.unishox2_decompress_simple(buffer, buffer.length);
+  return usx.unishox2_decompress_simple(buffer, buffer.length)
 }
